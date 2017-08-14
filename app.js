43a6d11653e503bd33f1ed7632e87bd35fee40fa
tdostilio <tdostilio@gmail.com>
2017-08-14 15:33:48 -0400

    added styling and dynamic element creation

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var facilities = require('./routes/facilities');
+var members = require('./routes/members');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/facilities', facilities);
+app.use('/members', members);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/members.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+const db = require('../db');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    db.query(`
+        select * from cd.members;
+    `).then( (result) => {
+        res.render('members', { 
+            members: result
+        });
+    }); 
+});
+
+module.exports = router;
